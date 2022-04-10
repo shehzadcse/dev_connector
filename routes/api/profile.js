f34b1b7b2c77230b92fc59ba13c6efbfbe28faaf
Shehzad Rana <58845701+shehzadcse@users.noreply.github.com>
2022-04-11 00:45:33 +0530

    Completed Profile Create/Update along with get

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,116 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  Public
-router.get("/", (req, res) => {
-  res.send("Profile Route");
+// @route   GET api/profile/me
+// @desc    Get Logged In User's Profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      res.status(400).json({ msg: "No Profile Exists for this user" });
+      return;
+    }
+    res.json(profile);
+  } catch (error) {
+    res.status(500).send("Server Error");
+    console.error(error.message);
+  }
+  // res.send("Profile Route");
+});
+
+// @route   POST api/profile
+// @desc    Create / Update User's Profile
+// @access  Private
+
+router.post(
+  '/',
+  auth,
+  check('status', 'Status is required').notEmpty(),
+  check('skills', 'Skills is required').notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure the request
+    const {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      githubusername,
+      experience,
+      education,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      date
+    } = req.body;
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (skills) profileFields.skills = skills.split(',').map((skill) => skill.trim())
+    if (bio) profileFields.bio = bio;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (experience) profileFields.experience = experience;
+    if (education) profileFields.education = education;
+    // if (date) profileFields.date = date;
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+    try {
+      let profile = await Profile.findOne({ user: profileFields.user });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: profileFields.user },
+          { $set: profileFields },
+          { new: true })
+        return res.json(profile);
+      }
+      else {
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+      }
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error')
+    }
+
+
+
+  }
+);
+
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
