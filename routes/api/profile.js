302379c538d81d2bc2e1e36a8bbca60a0cc4440a
Shehzad Rana <58845701+shehzadcse@users.noreply.github.com>
2022-04-13 00:04:32 +0530

    Completed Add Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,50 @@ router.delete('/', auth, async (req, res) => {
   }
 
 });
+
+// @route    PUT api/profile/experience
+// @desc     Add Profile Experience
+// @access   Private
+router.put('/experience', [auth, [
+  check('title', 'Title is required').not().isEmpty(),
+  check('company', 'Company is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty()
+
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  // Get request Data
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  // Create Experience Array
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    profile.experience.unshift(newExp)
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error....');
+  }
+})
 module.exports = router;
